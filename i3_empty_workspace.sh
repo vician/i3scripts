ec8ec7e9e6376bf26fa20d52d98bf7819ae96e5f
Martin Vician <martin.vician@nic.cz>
2016-01-05 09:26:13 +0100

    named empty workspace

--- i3_empty_workspace.sh
+++ i3_empty_workspace.sh
@@ -7,4 +7,16 @@ WORKSPACES=$(seq -s '\n' 1 1 ${MAX_DESKTOPS})
 EMPTY_WORKSPACE=$( (i3-msg -t get_workspaces | tr ',' '\n' | grep num | awk -F:  '{print int($2)}' ; \
             echo -e ${WORKSPACES} ) | sort -n | uniq -u | head -n 1)
 
-i3-msg workspace ${EMPTY_WORKSPACE}
+CONFIG_FILE=$HOME/.i3/config
+if [ -f $CONFIG_FILE ]; then
+	grep "set \$w" $CONFIG_FILE
+	if [ $? -eq 0 ]; then
+		NAMED_EMPTY_WORKSPACE=$(grep "set \$w" $CONFIG_FILE | grep $EMPTY_WORKSPACE | awk '{print $3,$4}')
+	else
+		NAMED_EMPTY_WORKSPACE=$EMPTY_WORKSPACE
+	fi
+else
+	NAMED_EMPTY_WORKSPACE=$EMPTY_WORKSPACE
+fi
+
+i3-msg workspace ${NAMED_EMPTY_WORKSPACE}
